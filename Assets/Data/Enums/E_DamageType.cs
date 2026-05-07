85fd80dd9c4e3bc573f15f5a29732992db2a5333
Joshua Shulman-Cass <joshscgd@gmail.com>
2026-05-07 16:51:10 -0700

    all SO's have updated data, will make deck work now

--- Assets/Data/Enums/E_DamageType.cs
+++ Assets/Data/Enums/E_DamageType.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public enum E_DamageType 
 {
+    [Tooltip("Does not include any special properties")] Default,
     Fire,
     Water,
     Earth,
     Lightning,
     Poison,
     Ice,
+    Wind,
+    Magic,
 }
--- Assets/Data/ScriptableObjects/PSO_Card.cs
+++ Assets/Data/ScriptableObjects/PSO_Card.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SO_Card", menuName = "Scriptable Objects/SO_Card")]
 public class PSO_Card : ScriptableObject //i added a prefix of "P" for primary (im used to PDA's in unreal, which are primary data assets) 
 {
+    public enum E_CardType
+    {
+        Unit,
+        Spell,
+    }
+    //Visuals fields 
     [field: SerializeField, BoxGroup("Visuals")] public string Name { get; private set; } = "Card Name";
     [field: SerializeField, BoxGroup("Visuals")] public Sprite Illustration { get; private set; }
     [field: SerializeField, BoxGroup("Visuals")] public string Description { get; private set; } = "Card Description";
-    
-    [field: SerializeField, MinValue(0), BoxGroup("Stats")] public float Health { get; private set; }
-    [field: SerializeField, MinValue(0), BoxGroup("Stats")] public float Damage { get; private set; }
+   
+    //Stat fields 
     [field: SerializeField, MinValue(0), BoxGroup("Stats")] public int Cost { get; private set; }
-    [field: SerializeField,BoxGroup("Stats")] public E_DamageType DamageType { get; private set; }
+    [field: SerializeField, BoxGroup("Stats")] public E_CardType CardType { get; private set;}
+    
+    [field: SerializeField, MinValue(0), BoxGroup("Stats"), ShowIf("CardType", E_CardType.Unit)] 
+        public float Damage { get; private set; }
+    
+    [field: SerializeField, MinValue(0), BoxGroup("Stats"), ShowIf("CardType", E_CardType.Unit)] 
+        public float Health { get; private set; }
+    
+    [field: SerializeField,BoxGroup("Stats"), ShowIf("CardType", E_CardType.Unit)] 
+        public E_DamageType DamageType { get; private set; }
 }
