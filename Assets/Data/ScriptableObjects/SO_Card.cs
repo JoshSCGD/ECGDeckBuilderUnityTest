eb543a83391353e7cfcb510865ccc36b6c8cba92
Joshua Shulman-Cass <joshscgd@gmail.com>
2026-05-07 14:30:49 -0700

    card sprites are in

--- Assets/Data/ScriptableObjects/SO_Card.cs
+++ Assets/Data/ScriptableObjects/SO_Card.cs
@@ -1,14 +1,16 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "SO_Card", menuName = "Scriptable Objects/SO_Card")]
 public class SO_Card : ScriptableObject
 {
-    //ensured that all vars are public getters and private setters to prevent potential error down the line
-    public string Name { get; private set; } = "Card Name";
-    [Min(0)] public int Cost { get; private set; } 
-    [Min(0)] public float Damage { get; private set; }
-    public E_DamageType DamageType { get; private set; } 
-    [Min(0)] public float Health { get; private set; } 
-    public Sprite Illustration { get; private set; }
-    public string Description { get; private set; } = "Card Description";
+    [field: SerializeField, BoxGroup("Visuals")] public string Name { get; private set; } = "Card Name";
+    [field: SerializeField, BoxGroup("Visuals")] public Sprite Illustration { get; private set; }
+    [field: SerializeField, BoxGroup("Visuals")] public string Description { get; private set; } = "Card Description";
+    
+    [field: SerializeField, MinValue(0), BoxGroup("Stats")] public float Health { get; private set; }
+    [field: SerializeField, MinValue(0), BoxGroup("Stats")] public float Damage { get; private set; }
+    [field: SerializeField, MinValue(0), BoxGroup("Stats")] public int Cost { get; private set; }
+    [field: SerializeField,BoxGroup("Stats")] public E_DamageType DamageType { get; private set; }
+    
 }
